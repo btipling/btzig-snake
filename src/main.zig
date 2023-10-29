b0d9900af3817eab28e7eee551372c9a7883c095
btipling <bjorn@ambientchill.com>
2023-10-29 13:27:28 -0700

    drawing a triangle now

--- src/main.zig
+++ src/main.zig
@@ -44,6 +44,103 @@ pub fn main() !void {
         std.debug.print("Drawable size is {d}x{d}\n", .{ w, h });
     }
 
+    var vertices: [9]gl.Float = [9]gl.Float{
+        -0.7, -0.5, 0.0,
+        0.7,  -0.5, 0.0,
+        0.0,  0.7,  0.0,
+    };
+
+    var VAO: gl.Uint = undefined;
+    gl.genVertexArrays(1, &VAO);
+    gl.bindVertexArray(VAO);
+    var VBO: gl.Uint = undefined;
+    gl.genBuffers(1, &VBO);
+    gl.bindBuffer(gl.ARRAY_BUFFER, VBO);
+    gl.bufferData(gl.ARRAY_BUFFER, vertices.len * @sizeOf(gl.Float), &vertices, gl.STATIC_DRAW);
+    gl.vertexAttribPointer(0, 3, gl.FLOAT, gl.FALSE, 3 * @sizeOf(gl.Float), null);
+    gl.enableVertexAttribArray(0);
+    var e = gl.getError();
+    if (e != gl.NO_ERROR) {
+        std.debug.print("error: {d}\n", .{e});
+        return;
+    }
+
+    var vertexShaderSource: [:0]const u8 = @embedFile("shaders/segment.vs");
+    std.debug.print("vertexShaderSource: {s}\n", .{vertexShaderSource.ptr});
+    var vertexShader: gl.Uint = gl.createShader(gl.VERTEX_SHADER);
+    gl.shaderSource(vertexShader, 1, &[_][*c]const u8{vertexShaderSource.ptr}, null);
+    gl.compileShader(vertexShader);
+    var success: gl.Int = 0;
+    gl.getShaderiv(vertexShader, gl.COMPILE_STATUS, &success);
+    if (success == 0) {
+        var infoLog: [512]u8 = undefined;
+        var logSize: gl.Int = 0;
+        gl.getShaderInfoLog(vertexShader, 512, &logSize, &infoLog);
+        var i: usize = @intCast(logSize);
+        std.debug.print("ERROR::SHADER::VERTEX::COMPILATION_FAILED\n{s}\n", .{infoLog[0..i]});
+        return;
+    } else {
+        var infoLog: [512]u8 = undefined;
+        var logSize: gl.Int = 0;
+        gl.getShaderInfoLog(vertexShader, 512, &logSize, &infoLog);
+        var i: usize = @intCast(logSize);
+        std.debug.print("INFO::SHADER::VERTEX::LINKING_SUCCESS\n{s}\n", .{infoLog[0..i]});
+    }
+
+    var fragmentShaderSource: [:0]const u8 = @embedFile("shaders/segment.fs");
+    std.debug.print("fragmentShaderSource: {s}\n", .{fragmentShaderSource.ptr});
+    var fragmentShader: gl.Uint = gl.createShader(gl.FRAGMENT_SHADER);
+    gl.shaderSource(fragmentShader, 1, &[_][*c]const u8{fragmentShaderSource.ptr}, null);
+    gl.compileShader(fragmentShader);
+    gl.getShaderiv(fragmentShader, gl.COMPILE_STATUS, &success);
+    if (success == 0) {
+        var infoLog: [512]u8 = undefined;
+        var logSize: gl.Int = 0;
+        gl.getShaderInfoLog(fragmentShader, 512, &logSize, &infoLog);
+        var i: usize = @intCast(logSize);
+        std.debug.print("ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n{s}\n", .{infoLog[0..i]});
+        return;
+    } else {
+        var infoLog: [512]u8 = undefined;
+        var logSize: gl.Int = 0;
+        gl.getShaderInfoLog(vertexShader, 512, logSize, &infoLog);
+        var i: usize = @intCast(logSize);
+        std.debug.print("INFO::SHADER::FRAGMENT::LINKING_SUCCESS\n{s}\n", .{infoLog[0..i]});
+    }
+
+    var shaderProgram: gl.Uint = gl.createProgram();
+    gl.attachShader(shaderProgram, vertexShader);
+    gl.attachShader(shaderProgram, fragmentShader);
+    e = gl.getError();
+    if (e != gl.NO_ERROR) {
+        std.debug.print("error: {d}\n", .{e});
+        return;
+    }
+    gl.linkProgram(shaderProgram);
+    gl.getProgramiv(shaderProgram, gl.LINK_STATUS, &success);
+    if (success == 0) {
+        var infoLog: [512]u8 = undefined;
+        var logSize: gl.Int = 0;
+        gl.getProgramInfoLog(shaderProgram, 512, &logSize, &infoLog);
+        var i: usize = @intCast(logSize);
+        std.debug.print("ERROR::SHADER::PROGRAM::LINKING_FAILED\n{s}\n", .{infoLog[0..i]});
+        return;
+    } else {
+        var infoLog: [512]u8 = undefined;
+        var logSize: gl.Int = 0;
+        gl.getProgramInfoLog(shaderProgram, 512, &logSize, &infoLog);
+        var i: usize = @intCast(logSize);
+        std.debug.print("INFO::SHADER::PROGRAM::LINKING_SUCCESS {d}\n{s}\n", .{ i, infoLog[0..i] });
+    }
+    gl.deleteShader(vertexShader);
+    gl.deleteShader(fragmentShader);
+    e = gl.getError();
+    if (e != gl.NO_ERROR) {
+        std.debug.print("error: {d}\n", .{e});
+        return;
+    }
+    std.debug.print("program set up \n", .{});
+
     main_loop: while (true) {
         var event: sdl.Event = undefined;
         while (sdl.pollEvent(&event)) {
@@ -54,6 +151,25 @@ pub fn main() !void {
             }
         }
         gl.clearBufferfv(gl.COLOR, 0, &[_]f32{ 0.2, 0.4, 0.8, 1.0 });
+
+        gl.useProgram(shaderProgram);
+        e = gl.getError();
+        if (e != gl.NO_ERROR) {
+            std.debug.print("error: {d}\n", .{e});
+            return;
+        }
+        gl.bindVertexArray(VAO);
+        e = gl.getError();
+        if (e != gl.NO_ERROR) {
+            std.debug.print("error: {d}\n", .{e});
+            return;
+        }
+        gl.drawArrays(gl.TRIANGLES, 0, 3);
+        if (e != gl.NO_ERROR) {
+            std.debug.print("error: {d}\n", .{e});
+            return;
+        }
+
         sdl.gl.swapWindow(window);
     }
 }
