fb93ab3048faefdfb1a34989d09634e3192aeb99
btipling <bjorn@ambientchill.com>
2023-11-18 11:21:18 -0800

    add an up and down head

--- src/game/start.zig
+++ src/game/start.zig
@@ -66,6 +66,8 @@ pub fn start() !void {
     var segRight = try segment.Segment.initRight();
     var headRight = try head.Head.initRight();
     var headLeft = try head.Head.initLeft();
+    var headUp = try head.Head.initUp();
+    var headDown = try head.Head.initDown();
     var foodItem = try food.Food.init();
     var gameGrid = grid.Grid.init(cfg.grid_size);
     var bg = try background.Background.init(gameGrid.size);
@@ -104,8 +106,12 @@ pub fn start() !void {
         var headPosY: gl.Float = try gameGrid.indexToGridPosition(headCoords.y);
         if (gameState.direction == .Left) {
             try headLeft.draw(headPosX, headPosY, gameState.grid.scaleFactor);
-        } else {
+        } else if (gameState.direction == .Right) {
             try headRight.draw(headPosX, headPosY, gameState.grid.scaleFactor);
+        } else if (gameState.direction == .Up) {
+            try headUp.draw(headPosX, headPosY, gameState.grid.scaleFactor);
+        } else {
+            try headDown.draw(headPosX, headPosY, gameState.grid.scaleFactor);
         }
         for (gameState.segments.items[1..], 0..) |coords, i| {
             var posX: gl.Float = try gameGrid.indexToGridPosition(coords.x);
